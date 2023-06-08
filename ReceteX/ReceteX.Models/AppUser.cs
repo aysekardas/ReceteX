439c7cb2dac8072fba9f73c2a29eea8cac8fcffb
aysekardas <aysekardas7@gmail.com>
2023-06-08 17:51:03 +0300

    Base model and appuser class

---
+++ ReceteX/ReceteX.Models/AppUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceteX.Models
+{
+    public class AppUser : BaseModel
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Gsm { get; set; }
+        public string? TCKN { get; set; }
+        //e-signature pin code
+        public string? PinCode { get; set; }
+        //İstanbul = 34 
+        public int? CityCode { get; set;}
+        //numbers and letters mixed
+        public string? MedulaPassword { get; set; }
+    }
+}
