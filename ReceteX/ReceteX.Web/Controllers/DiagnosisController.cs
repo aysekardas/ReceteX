c6bde575854f17aede2605cd9f896818ed931539
aysekardas <aysekardas7@gmail.com>
2023-06-11 04:17:59 +0300

    anasayfa için ilk adımlar

---
+++ ReceteX/ReceteX.Web/Controllers/DiagnosisController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReceteX.Web.Controllers
+{
+    public class DiagnosisController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
